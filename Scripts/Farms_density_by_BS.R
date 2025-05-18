47c891ef92e22907de3322879c0707f19949edeb
lucianolasala <lucianolasala@yahoo.com.ar>
2025-05-18 01:05:38 -0300

    1

--- Scripts/Farms_density_by_BS.R
+++ Scripts/Farms_density_by_BS.R
@@ -38,125 +38,68 @@ nodes <- read.csv("D:/CIC/Analisis/Wild_boar_pigs_interaction/Datos/nodes.csv", 
 #-------------------------------------------------------------------------------
 
 BS_1 <- subset(nodes, BS == "High", select = c(Lat, Lon, BS))
+which(is.na(BS_1$Lat|BS_2$Lon))
 
 # Create a new variable that includes the difference between latitude and longitude 
 # to make sure that each farm record is unique
 
 BS_1$COORDS_COMB <- BS_1$Lat-BS_1$Lon; head(BS_1)
 
+# Check
 length(BS_1$COORDS_COMB) 
 length(unique(BS_1$COORDS_COMB))
-
 rep_coords <- BS_1[duplicated(BS_1$COORDS_COMB),] 
 length(rep_coords$COORDS_COMB)
 
-# Crete a SpatialPointsDataFrame to save as ESRI shapefile 
-
+# Crete a SpatialPointsDataFrame and save as ESRI shapefile 
 coords <- BS_1[,c("Lon", "Lat")]
-head(coords)
-is.data.frame(coords)
 which(is.na(coords$LAT|coords$LONG))
 
 data <- as.data.frame(BS_1$BS)
-head(data)
 colnames(data) <- "BS"
-is.data.frame(data)
 
 crs <- CRS("+init=epsg:4326")
 spdf <- SpatialPointsDataFrame(coords = coords, data = data, proj4string = crs)
 
-class(spdf)
-spdf@coords
-spdf@data
-
 writeOGR(spdf,"D:/CIC/Analisis/Wild_boar_pigs_interaction/Vectors","BS_1", driver = "ESRI Shapefile", overwrite_layer = T)
 
-# Read raster
-
-rm(list=ls(all=TRUE))
-
+# Read Argentina's raster created above
 arg_ras <- raster("D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/Raster_10km.asc")
 
 # Function to count farms/pixel
-
 pointcount = function(arg_ras, spdf){
   
   # Raster as input:
   r2 = arg_ras
   
-  # Get the cell index for each point and make a table:
+  # Get cell index for each point and make a table:
   counts = table(cellFromXY(arg_ras, spdf))
   
-  # Fill in the raster with the counts from the cell index:
+  # Fill in raster with counts from the cell index:
   r2[as.numeric(names(counts))] = counts
   return(r2)
 }
 
-# Result and raster exploration
-
+# Result
 BS1 = pointcount(arg_ras, spdf)
-plot(BS1)
-
 writeRaster(BS1, filename = "D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/BS1", format = "ascii", overwrite = TRUE)
-
-BS1 <- raster("D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/BS1.asc")
-str(BS1)
-
-setMinMax(BS1) 
-
-table(BS1[], useNA = "always")
-
-# Lengths
-length(which(BS1[]==0))
-length(which(BS1[]==1))   
-length(which(BS1[]==2)) 
-
-minValue(BS1)  
-maxValue(BS1)   
-
-BS1@data@min
-BS1@data@max   
-
-xyFromCell(BS1, 1)
-
-hist(BS1)
-hist(BS1[BS1!=0])  
-sum(values(BS1!=0), na.rm = TRUE)  
-canProcessInMemory(BS1)
-length(which(values(BS1) %in% c(1,2)))
-
-# Frequency table
-table(BS1[])
-
 
 #-------------------------------------------------------------------------------
 # Subset BS2
 #-------------------------------------------------------------------------------
 
 BS_2 <- subset(nodes, BS == "Medium", select = c(Lat, Lon, BS))
-head(BS_2)
-length(BS_2$Lat)
-
 which(is.na(BS_2$Lat|BS_2$Lon))
 
-# Create a new variable that combines lat and lon (difference) to make sure that 
-# each farm record is unique.
+# Create a new variable that includes the difference between latitude and longitude 
+# to make sure that each farm record is unique
 
 BS_2$COORDS_COMB <- BS_2$Lat-BS_2$Lon
 head(BS_2)
-
-length(BS_2$COORDS_COMB)
-length(unique(BS_2$COORDS_COMB))
-
-rep_coords <- BS_2[duplicated(BS_2$COORDS_COMB),] 
-length(rep_coords$COORDS_COMB)
   
-# Crete a SpatialPointsDataFrame to save as ESRI shapefile 
+# Crete a SpatialPointsDataFrame and save as ESRI shapefile 
 
 coords <- BS_2[,c("Lon", "Lat")]
-is.data.frame(coords)
-head(coords)
-
 which(is.na(coords$LAT|coords$LONG))
 
 datos <- as.data.frame(BS_2[,3])
@@ -164,16 +107,11 @@ head(datos)
 colnames(datos) <- "BS"
 
 crs <- CRS("+init=epsg:4326")
-
 spdf <- SpatialPointsDataFrame(coords = coords, data = datos, proj4string = crs)
-
-class(spdf) 
-spdf@coords
-spdf@data
 
 writeOGR(spdf,"D:/CIC/Analisis/Wild_boar_pigs_interaction/Vectors","BS_2", driver = "ESRI Shapefile", overwrite_layer = T)
 
-# Read raster
+# Read Argentina's raster created above
 arg_ras <- raster("D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/Raster_10km.asc")
 
 # Function to count farms/pixel
@@ -190,32 +128,9 @@ pointcount = function(arg_ras, spdf){
   return(r2)
 }
 
-
-# Result and raster exploration
+# Result
 BS2 = pointcount(arg_ras, spdf)
-plot(BS2)
-
 writeRaster(BS2, "D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/BS2", format = "ascii", overwrite = TRUE)
-
-BS2 <- raster("D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/BS2.asc")
-class(BS2)
-
-setMinMax(BS2)
-
-BS2@data@values[1]
-xyFromCell(BS2, 1)
-
-hist(BS2)  
-
-hist(BS2[BS2!=0]) 
-
-sum(values(BS2!=0), na.rm = TRUE)  
-
-length(which(values(BS2) %in% c(0,1,2,3,4))) 
-
-# Frequency table
-
-table(BS2[])
 
 #-------------------------------------------------------------------------------
 # Subset BS3
@@ -227,10 +142,9 @@ BS_3 <- subset(nodes, BS == "Low", select = c(Lat, Lon, BS))
 length(BS_3$Lat)
 which(is.na(BS_3$Lat|BS_3$Lon))
 
-# Create a new variable that combines lat and lon (difference) to make sure that 
-# each farm record is unique. 
+# Create a new variable that includes the difference between latitude and longitude 
+# to make sure that each farm record is unique 
 BS_3$COORDS_COMB <- BS_3$Lat - BS_3$Lon
-head(BS_3)
 
 length(BS_3$COORDS_COMB)
 length(unique(BS_3$COORDS_COMB))
@@ -238,27 +152,20 @@ length(unique(BS_3$COORDS_COMB))
 rep_coords <- BS_3[duplicated(BS_3$COORDS_COMB),] 
 length(rep_coords$COORDS_COMB)
 
-# Nota. Do not delete repeated records. They are farms with same coordinates 
+# Note. Do not delete repeated records. They are farms with same coordinates 
 # because they were assigned to the centroid of a county 
 
 # Crete a SpatialPointsDataFrame to save as ESRI shapefile 
 coords <- BS_3[,c("Lon", "Lat")]
-is.data.frame(coords)
-head(coords)
 
 which(is.na(coords$Lat|coords$Lon))  # No hay NAs
 
 datos <- as.data.frame(BS_3[,c("BS")])
-head(datos)
 colnames(datos) <- "BS"
 
 crs <- CRS("+init=epsg:4326")
 
 spdf <- SpatialPointsDataFrame(coords = coords, data = datos, proj4string = crs)
-
-class(spdf)  # "SpatialPointsDataFrame"
-spdf@coords
-spdf@data
 
 writeOGR(spdf,"D:/CIC/Analisis/Wild_boar_pigs_interaction/Vectors","BS_3", driver = "ESRI Shapefile", overwrite_layer = T)
 
@@ -276,22 +183,6 @@ pointcount = function(arg_ras, spdf){
   return(r2)
 }
 
-# Result and raster exploratio
+# Result
 BS3 = pointcount(arg_ras, spdf)
-plot(BS3)
-
 writeRaster(BS3, filename = "D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/BS3", format = "ascii", overwrite = TRUE)
-
-BS3 <- raster("D:/CIC/Analisis/Wild_boar_pigs_interaction/Rasters/BS3.asc")
-class(BS3)
-
-setMinMax(BS3)
-hist(BS3)  
-
-hist(BS3[BS3!=0]) 
-sum(values(BS3!=0), na.rm = TRUE)
-length(which(values(BS3) %in% c(1:20)))
-
-# Frequancy table
-table(BS3[])
-
